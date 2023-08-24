bc14a0450e23c9ac0fd55a2db83a994cf97181d1
andrey-lawyer <andrey-1@ukr.net>
2023-08-24 17:37:01 +0300

    fix reset form

--- src/hooks/useHookForm.ts
+++ src/hooks/useHookForm.ts
@@ -1,23 +1,34 @@
 import { useForm } from "react-hook-form";
 
 import { FormData } from "@/types/formTypes";
+import { useEffect } from "react";
 
 export const useHookForms = () => {
 
 
   const {
     register,
+    formState,
     handleSubmit,
+    reset,
     formState: { errors },
   } =    useForm<FormData>({});
 
   const onSubmit = handleSubmit((data) => {
-    if (!data) console.log("error");
-    else if (data.phone) {
+   
+    if (data.phone) {
       data.phone = "+38" + data.phone;
     }
       console.log(data);
+      alert("The form was successfuly sent!");
+      
     
   });
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset();
+    }
+  }, [formState,  reset]);
+
   return { register, errors, onSubmit };
 };

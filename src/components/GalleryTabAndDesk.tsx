e979b4aac5791759880a2e81608192275e8d2415
andrey-lawyer <113498656+andrey-lawyer@users.noreply.github.com>
2025-10-08 13:38:20 +0300

    AI PR for Issue KAN-5

--- src/components/GalleryTabAndDesk.tsx
+++ src/components/GalleryTabAndDesk.tsx
@@ -1,21 +1,17 @@
 import { useRef } from "react";
 import Image from "next/image";
-
 import "swiper/css";
 import "swiper/css/navigation";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Swiper as SwiperCore } from "swiper/types";
 import { Navigation } from "swiper/modules";
-
 import data from "@/data/galleryData.json";
-
 import "@/sections/Gallery/gallery.css";
 
 export function GalleryTabAndDesk() {
   const swiperRef = useRef<SwiperCore>();
   return (
-    <>
+    <> 
       <Swiper
         slidesPerView={3}
         centeredSlides={true}
@@ -39,7 +35,7 @@ export function GalleryTabAndDesk() {
                 <div
                   className={`${
                     isActive ? "active_image" : "not_active_image"
-                  }   relative  `}
+                  }   relative  "}
                 >
                   <Image
                     src={el.url}
@@ -58,14 +54,15 @@ export function GalleryTabAndDesk() {
       <div className="absolute bottom-[59px] desk:bottom-[94px] z-10 w-[704px] desk:w-[1280px] flex justify-between text-white text-xxl_tab_nav_image font-thin pl-[37px] pb-[17px] pr-[38px] desk:px-6">
         <button
           type="button"
-          className="desk:w-[294px] desk:text-right"
-          onClick={() => swiperRef.current?.slidePrev()}
+          className="desk:w-[294px] desk:text-right bg-blue-500 hover:bg-blue-700 focus:bg-blue-700" /* Change button styles to blue */
+          onClick={() => swiperRef.current?.slidePrev()
+          }
         >
           BACK
         </button>
         <button
           type="button"
-          className="desk:w-[294px] desk:text-left"
+          className="desk:w-[294px] desk:text-left bg-blue-500 hover:bg-blue-700 focus:bg-blue-700"
           onClick={() => {
             return swiperRef.current?.slideNext();
           }}
